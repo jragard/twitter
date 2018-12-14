c08aa094fce07db1f62248db4eb9f453ded6a45c
Ryan <jragard@gmail.com>
2018-12-14 17:57:08 -0500

    cleaned up for pep8

--- twitter/forms.py
+++ twitter/forms.py
@@ -1,13 +1,16 @@
 from django import forms
 
+
 class SignupForm(forms.Form):
     username = forms.CharField(max_length=40)
     email = forms.EmailField()
     password = forms.CharField(widget=forms.PasswordInput())
 
+
 class LoginForm(forms.Form):
     username = forms.CharField(max_length=40)
     password = forms.CharField(widget=forms.PasswordInput())
+
 
 class AddTweet(forms.Form):
     body = forms.CharField(max_length=140)
